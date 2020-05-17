6b5d66857498d0cfb7ce3261e508af66d70344d5
Mihail <sidorov.mihail.maykop@mail.ru>
2020-05-17 20:09:31 +0300

    Прописал диалоговое окно для формы обратной связи.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,4 @@ require('./bem/controllers/app-controller');
 require('./bem/slider');
 require('./bem/feedback-form');
 require('./bem/advantages');
+require('./bem/modal-window');
---
+++ resources/js/bem/controllers/app-controller.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+    $('.header__order-call-btn').click(() => {
+        $('.modal-window').addClass('modal-window_show');
+    });
+});
